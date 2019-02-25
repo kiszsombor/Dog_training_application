10056e0ba6740c00d27205e2edd9f17a2bedf543
luciabajari <bajarilucia@Kgmail.com>
2019-02-25 18:19:33 +0100

    backend alap projekt

---
+++ backend/src/main/java/hu/elte/DogTrainingApplication/DogTrainingApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.DogTrainingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogTrainingApplication.class, args);
+	}
+
+}
