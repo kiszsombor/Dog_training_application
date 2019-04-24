d3bcfef18b57cde35ba49b06e4be056d70731583
juhibiu <juhasz.bianka@gmail.com>
2019-04-24 12:54:06 +0200

    axios

--- DogTraining-frontend/src/components/SeasonTickets.vue
+++ DogTraining-frontend/src/components/SeasonTickets.vue
@@ -1,2 +1,19 @@
 <template>
+<div class="kiskutya"></div>
 </template>
+
+<script>
+export default {
+    data() {
+        return{
+            count: count
+        }
+    },
+    methods: {
+        count() {
+            console.log(this.$store.state.count)
+            return this.$store.state.count
+        }
+    }
+}
+</script>
