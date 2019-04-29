87d49241307433105fa5a41780671e68a2991d51
Juhasz Bianka <juhasz.bianka@gmail.com>
2019-04-29 19:01:36 +0200

    trickPages bekötés

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,10 @@ import Router from 'vue-router'
 import Home from '@/router/views/Home'
 import LoginPage from '@/router/views/LoginPage'
 import SeasonTickets from '@/components/SeasonTickets'
+import TrickPage from '@/components/TrickPage'
+import BasicTricks from '@/components/BasicTricks'
+import IntermediateTricks from '@/components/IntermediateTricks'
+import AdvancedTricks from '@/components/AdvancedTricks'
 import Page2 from '@/router/views/Page2'
 import Page3 from '@/router/views/Page3'
 import Page4 from '@/router/views/Page4'
@@ -28,6 +32,26 @@ export default new Router({
       path: '/SeasonTickets',
       name: 'SeasonTickets',
       component: SeasonTickets
+    },
+    {
+      path: '/TrickPage',
+      name: 'TrickPage',
+      component: TrickPage
+    },
+    {
+      path: '/TrickPage/BasicTricks',
+      name: 'BasicTricks',
+      component: BasicTricks
+    },
+    {
+      path: '/TrickPage/IntermediateTricks',
+      name: 'IntermediateTricks',
+      component: IntermediateTricks
+    },
+    {
+      path: '/TrickPage/AdvancedTricks',
+      name: 'AdvancedTricks',
+      component: AdvancedTricks
     },
     {
       path: '/Page2',
