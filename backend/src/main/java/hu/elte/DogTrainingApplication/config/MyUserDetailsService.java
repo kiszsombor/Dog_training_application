d1c5a5a4161ba27c9ded6b5f1edb46e0457a3cc1
luciabajari <bajarilucia@gmail.com>
2019-05-19 19:05:05 +0200

    security

--- backend/src/main/java/hu/elte/DogTrainingApplication/config/MyUserDetailsService.java
+++ backend/src/main/java/hu/elte/DogTrainingApplication/config/MyUserDetailsService.java
@@ -1,41 +1,41 @@
-//package hu.elte.DogTrainingApplication.config;
-//
-//import hu.elte.DogTrainingApplication.entities.Trainer;
-//import hu.elte.DogTrainingApplication.repository.TrainerRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.HashSet;
-//import java.util.Optional;
-//import java.util.Set;
-//
-//@Service
-//public class MyUserDetailsService implements UserDetailsService {
-//
-//    @Autowired
-//    private TrainerRepository repository;
-//
-//    @Autowired
-//    private AutenticatedTrainer authenticated;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-//        Optional<Trainer> oUser = repository.findByUsername(userName);
-//        if (!oUser.isPresent()) {
-//            throw new UsernameNotFoundException(userName);
-//        }
-//        Trainer trainer = oUser.get();
-//        authenticated.setTrainer(trainer);
-//        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-//        System.out.println("Trainer:"+trainer);
-//        grantedAuthorities.add(new SimpleGrantedAuthority(trainer.getRole().toString()));
-//
-//        return new org.springframework.security.core.userdetails.User(trainer.getName(), trainer.getPassword(),
-//                grantedAuthorities);
-//    }
-//}
+package hu.elte.DogTrainingApplication.config;
+
+import hu.elte.DogTrainingApplication.entities.Trainer;
+import hu.elte.DogTrainingApplication.repository.TrainerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private TrainerRepository repository;
+
+    @Autowired
+    private AutenticatedTrainer authenticated;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<Trainer> oUser = repository.findByUsername(userName);
+        if (!oUser.isPresent()) {
+            throw new UsernameNotFoundException(userName);
+        }
+        Trainer trainer = oUser.get();
+        authenticated.setTrainer(trainer);
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        System.out.println("Trainer:"+trainer);
+        grantedAuthorities.add(new SimpleGrantedAuthority(trainer.getRole().toString()));
+
+        return new org.springframework.security.core.userdetails.User(trainer.getName(), trainer.getPassword(),
+                grantedAuthorities);
+    }
+}
--- backend/src/main/java/hu/elte/DogTrainingApplication/config/SecurityConfig.java
+++ backend/src/main/java/hu/elte/DogTrainingApplication/config/SecurityConfig.java
@@ -1,4 +1,86 @@
-//package hu.elte.DogTrainingApplication.config;
+package hu.elte.DogTrainingApplication.config;
+//
+//    @Autowired
+//    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
+//
+//        auth
+//            .inMemoryAuthentication()
+//            .withUser("user").password("{noop}user").roles(USER)
+//            .and()
+//            .withUser("admin").password("{noop}admin").roles(ADMIN,USER);
+//    }
+
+
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .cors()
+//                .and()
+//                .csrf().disable()
+//                .authorizeRequests()
+////                .antMatchers("/dog/**", "/trainer/registration", "/trainer/login","season_tickets/**","trick/**","trainer/**").permitAll()   // important!
+//                .anyRequest().permitAll()
+//                //.anyRequest().authenticated()
+//                .and()
+//                .httpBasic()
+//                .authenticationEntryPoint(getBasicAuthEntryPoint())
+//                .and()
+//                .headers()      // important!
+//                .frameOptions().disable()
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//    }
+//
+//    @Bean
+//    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint(){
+//        return new CustomBasicAuthenticationEntryPoint();
+//    }
+//
+//    @Autowired
+//    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .userDetailsService(userDetailsService)
+//                .passwordEncoder(passwordEncoder());
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Autowired
+//    protected void configureAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .userDetailsService(userDetailsService)
+//                .passwordEncoder(passwordEncoder());
+//    }
+//
+//}
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    private static final String USER="USER";
+//    private static final String ADMIN="ADMIN";
 //
 //    @Autowired
 //    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
@@ -21,9 +103,8 @@
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
-//                .antMatchers("/dog/**", "/trainer/registration", "/trainer/login","season_tickets/**","trick/**","trainer/**").permitAll()   // important!
-                .anyRequest().permitAll()
-                //.anyRequest().authenticated()
+                .antMatchers("/dog/**", "/trainer/registration", "/trainer/login","season_tickets/**","tricks/**").permitAll()   // important!
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic()
                 .authenticationEntryPoint(getBasicAuthEntryPoint())
@@ -63,112 +144,43 @@
 
 
 //@Configuration
-//@EnableWebSecurity
 //@EnableGlobalMethodSecurity(securedEnabled = true)
+//@EnableWebSecurity
 //public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-////    private static final String USER="USER";
-////    private static final String ADMIN="ADMIN";
-////
-////    @Autowired
-////    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
-////
-////        auth
-////            .inMemoryAuthentication()
-////            .withUser("user").password("{noop}user").roles(USER)
-////            .and()
-////            .withUser("admin").password("{noop}admin").roles(ADMIN,USER);
-////    }
-//
+//    private static final String USER="USER";
+//    private static final String ADMIN="ADMIN";
 //
 //    @Autowired
-//    private UserDetailsService userDetailsService;
+//    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
+//
+//        auth
+//            .inMemoryAuthentication()
+//            .withUser("user").password("{noop}user").roles(USER)
+//            .and()
+//            .withUser("admin").password("{noop}admin").roles(ADMIN,USER);
+//    }
+//
 //
 //    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .cors()
-//                .and()
-//                .csrf().disable()
+//    protected  void configure(HttpSecurity httpSecurity) throws Exception{
+//        httpSecurity
 //                .authorizeRequests()
-//                .antMatchers("/dog/**", "/trainer/registration", "/trainer/login","season_tickets/**","tricks/**").permitAll()   // important!
-//                .anyRequest().authenticated()
+//                    .antMatchers("/dog/**").hasRole(USER)
+//                    .antMatchers("/trainer/**").hasRole(ADMIN)
+//                    .anyRequest().authenticated()
 //                .and()
-//                .httpBasic()
-//                .authenticationEntryPoint(getBasicAuthEntryPoint())
+//                    .formLogin().permitAll()
 //                .and()
-//                .headers()      // important!
-//                .frameOptions().disable()
+//                    .httpBasic()
 //                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//                .logout().logoutSuccessUrl("/login?logout").permitAll();
+//
 //    }
 //
-//    @Bean
-//    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint(){
-//        return new CustomBasicAuthenticationEntryPoint();
-//    }
-//
-//    @Autowired
-//    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .userDetailsService(userDetailsService)
-//                .passwordEncoder(passwordEncoder());
-//    }
 //
 //    @Bean
 //    public BCryptPasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder();
 //    }
 //
-//    @Autowired
-//    protected void configureAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .userDetailsService(userDetailsService)
-//                .passwordEncoder(passwordEncoder());
-//    }
-//
 //}
-//
-//
-////@Configuration
-////@EnableGlobalMethodSecurity(securedEnabled = true)
-////@EnableWebSecurity
-////public class SecurityConfig extends WebSecurityConfigurerAdapter {
-////    private static final String USER="USER";
-////    private static final String ADMIN="ADMIN";
-////
-////    @Autowired
-////    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
-////
-////        auth
-////            .inMemoryAuthentication()
-////            .withUser("user").password("{noop}user").roles(USER)
-////            .and()
-////            .withUser("admin").password("{noop}admin").roles(ADMIN,USER);
-////    }
-////
-////
-////    @Override
-////    protected  void configure(HttpSecurity httpSecurity) throws Exception{
-////        httpSecurity
-////                .authorizeRequests()
-////                    .antMatchers("/dog/**").hasRole(USER)
-////                    .antMatchers("/trainer/**").hasRole(ADMIN)
-////                    .anyRequest().authenticated()
-////                .and()
-////                    .formLogin().permitAll()
-////                .and()
-////                    .httpBasic()
-////                .and()
-////                .logout().logoutSuccessUrl("/login?logout").permitAll();
-////
-////    }
-//
-//
-////    @Bean
-////    public BCryptPasswordEncoder passwordEncoder() {
-////        return new BCryptPasswordEncoder();
-////    }
-//
-////}
